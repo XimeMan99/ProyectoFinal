23e399a01776c6ddee7a4dcab20ced6ea0332b23
LorenaGBeltran <43042948+LorenaGBeltran@users.noreply.github.com>
2018-09-06 21:04:21 -0600

    Update BaseDatos.java
    Se añaden los métodos llenarEstudiante() y el atributo de  tipo ArrayList como una colección de  los usuarios

--- BaseDatos.java
+++ BaseDatos.java
@@ -1,18 +1,20 @@
 
-public class BaseDatos {
+import java.util.ArrayList;
   public class BaseDatos {
 	
 	//atributos
 	private Estudiante alumno;
 	private String nombreUsuario;
 	private String contra;
+	 private ArrayList<Estudiante> usuarios;
 
 	//constructor con parametros
-	public BaseDatos(String nombreUusuario, String contra, Estudiante alumno) 
+	public BaseDatos(String nombreUusuario, String contra, Estudiante alumno,  ArrayList<Estudiante> usuarios) 
 	{
 		this.nombreUsuario = nombreUsuario;
 		this.contra = contra;
 		this.alumno = alumno;
+		this.usuarios = usuarios;
 	}
 	
 	//constructor sin parametros
@@ -21,6 +23,7 @@ public class BaseDatos {
 		nombreUsuario = " ";
 		contra = " ";
 		alumno = new Estudiante();
+		ArrayList<Estudiante>usuarios = new ArrayList<Estudiante>();
 	}
 
 	
@@ -40,9 +43,32 @@ public class BaseDatos {
 	public void setContra(String contra) {
 		this.contra = contra;
 	}
+	public ArrayList getUsuarios() {
+		return usuarios;
+	}
+	public void setUsuarios(ArrayList<Estudiante>usuarios) {
+		this.usuarios = usuarios;
+	}
 
 	//Llenar atributos de la clase
 	public void llenarBaseDatos(String nombreUsuario, String contra) 
+	{
+		alumno.setNombreUsuario(nombreUsuario);
+		alumno.setContra(contra);
+	}
+	public void llenarEsudiante(String nombre,int carnet,String correo, boolean estatus,String nombreUsuario,String contra) {
+		alumno = new Estudiante();
+		alumno.setNombre(nombre);
+		alumno.setCarnet(carnet);
+		alumno.setCorreo(correo);
+		alumno.setEstatus(estatus);
+		alumno.setNombreUsuario(nombreUsuario);
+		alumno.setContra(contra);
+		
+		
+		
+	//Llenar atributos de la clase
+ 	public void llenarBaseDatos(String nombreUsuario, String contra) 
 	{
 		alumno.setNombreUsuario(nombreUsuario);
 		alumno.setContra(contra);
